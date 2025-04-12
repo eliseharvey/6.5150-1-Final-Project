a7a02f0416a22a8b72e87f388f2e4c0b95bd42c0
Eli Scharf <eli@Elis-MBP.fios-router.home>
2025-04-12 19:36:13 -0400

    adding test file for easy game testing

---
+++ test-game.scm
@@ -0,0 +1,11 @@
+(load "path-to/6.5150-1-Final-Project/cards.scm")
+(load "path-to/6.5150-1-Final-Project/gamestate.scm")
+(load "path-to/6.5150-1-Final-Project/utils.scm")
+(load "path-to/6.5150-1-Final-Project/game-commands.scm")
+
+;; Initialize a game
+(define game-state (make-game-state (make-deck) '() '(0 0 0 0)))
+
+(set! game-state (pick-card game-state))
+
+(set! game-state (place-card game-state 2))
