f788b363930481b22a9d8fcca815535ae3a1ba67
Elise Harvey <eliseharvey@dhcp-10-31-160-29.dyn.mit.edu>
2025-04-26 17:16:39 -0400

    working umpire yay

--- umpire.scm
+++ umpire.scm
@@ -1,42 +1,25 @@
 ;; umpire.scm
 
-;; Primitive rules
-
+;; primitive rules
 (define (valid-pick? state)
-  (not (null? (cdr (get-deck state)))))
-
+  (and (null? (cdr (get-player-hand state))) ; empty hand
+       (not (null? (cdr (get-deck state)))))) ; non-empty deck
+ 
 (define (valid-place-in-stack? state bucket-index)
-  (let ((hand (cdr (get-player-hand state))))
-    (and (not (null? hand))
-         (let* ((card (car hand))
-                (value (card-value card))
-                (current (get-bucket-value state bucket-index)))
-           (<= (+ value current) 21)))))
-
-;; Logical combinators
-
-(define (rule-or r1 r2)
-  (lambda (state)
-    (or (r1 state) (r2 state))))
-
-;; Build umpire
-
+  (and (not (null? (cdr (get-player-hand state)))) ; non-empty hand
+       (and (>= bucket-index 0) (<= bucket-index 3)))) ; bucket in [0, 1, 2, 3]
+  
+;; build umpire
 (define (build-umpire pick-rule place-rule)
-  (display "\nThis is being called\n")
-  (lambda (state move-type)
+  (lambda (state move-type . args)
     (cond
      ((string=? move-type "pick") (pick-rule state))
-     ((string=? move-type "place")
-      (let loop ((i 0))
-        (cond
-         ((= i 4) #f) ;; no stack can take it
-         ((valid-place-in-stack? state i) #t)
-         (else (loop (+ i 1))))))
+     ((string=? move-type "place") (place-rule state (car args)))
      (else
       (begin
         (display "Invalid move type to umpire.\n")
         #f)))))
 
-;; Default rules: can pick if deck not empty, can place if any stack accepts
+;; default rules: can pick if deck not empty, can place if any stack accepts
 (define umpire
   (build-umpire valid-pick? valid-place-in-stack?))
